c64d186a1f6ed2ea9c24c00d50bdcb5d33ab58db
Robby Clements <rclements@isotope11.com>
2010-04-26 22:29:34 -0500

    removed problem with double snippet id on codeblock migration

--- db/migrate/20100126150602_snippet_id.rb
+++ db/migrate/20100126150602_snippet_id.rb
@@ -1,11 +1,9 @@
 class SnippetId < ActiveRecord::Migration
   def self.up
     add_column :images, :snippet_id, :integer
-    add_column :code_blocks, :snippet_id, :integer
   end
 
   def self.down
     remove_column :images, :snippet_id
-    remove_column :code_blocks, :snippet_id
   end
 end
